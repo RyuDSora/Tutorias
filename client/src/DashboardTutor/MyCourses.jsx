705e3a8d60a2d3dea36f38a1e7c8ea5ec71a2061
Luis Matamoros <47126608+RyuDSora@users.noreply.github.com>
2024-07-25 01:05:42 -0600

    tutor-cursos ya funcional

--- client/src/DashboardTutor/MyCourses.jsx
+++ client/src/DashboardTutor/MyCourses.jsx
@@ -1,34 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Button, Modal, ListGroup } from 'react-bootstrap';
-import { UriCursos } from '../components/Urls';
+import { UriCursos, uritutorsubject, uritutor } from '../components/Urls';
+import { confirmAlert } from 'react-confirm-alert';
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
 
 const MyCourses = () => {
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
   const [selectedCourse, setSelectedCourse] = useState(null);
-  const [courses, setCourses] = useState([
-    { id: 1, name: 'Matemáticas', description: 'Aprende los fundamentos de las matemáticas.' },
-    { id: 2, name: 'Física', description: 'Descubre los principios de la física.' },
-    { id: 3, name: 'Inglés', description: 'Mejora tus habilidades en el idioma inglés.' },
-  ]);
+  const [courses, setCourses] = useState([]);
   const [cursosSelect, setCursosSelect] = useState([]);
-
+  const [tutorId,setTutorId]= useState(0);
+  const [add, setAdd] = useState(false);
+  const pre = '/images/';
   useEffect(() => {
-    const fetchSubject = async () => {
-      if (Cookies.get('session')) {
-        try {
-          const response = await axios.get(UriCursos);
-          setCursosSelect(response.data);
-        } catch (error) {
-          console.error(error);
-        }
+    const fetchCurses = async () => {
+      try {
+        const response = await axios.get(UriCursos);
+        const cursos = response.data;
+
+        // Filtra los cursos disponibles eliminando los que ya están en `courses`
+        const filteredCursos = cursos.filter(curso =>
+          !courses.some(course => course.id_subject === curso.id)
+        );
+
+        setCursosSelect(filteredCursos);
+      } catch (error) {
+        console.log(error);
       }
     };
 
+    fetchCurses();
+  }, [courses]);
+
+  useEffect(() => {
+    const fetchSubject = async () => {
+        try {
+          //recupera el id del usuario de las cookies
+          const userId = Cookies.get('UserId');
+          //recupera los datos del tutor de la base de datos
+          const responsetutor = await axios.get(`${uritutor}/${userId}`);
+          //recupera el idtutor del response
+          const idTutor = responsetutor.data.id;
+          setTutorId(responsetutor.data.id);
+
+          //recupera los cursos por tutor
+          const responseTutorSubjects = await axios.get(`${uritutorsubject}/${idTutor}`);
+          const tutorSubjects = responseTutorSubjects.data;
+          //recupera la informacion de cada curso segun la lista anterior y lo agrega como nueva informacion al mismo json
+          const coursesData = await Promise.all(
+            tutorSubjects.map(async (tutorSubject) => {
+              const courseResponse = await axios.get(`${UriCursos}/${tutorSubject.id_subject}`);
+              return {
+                ...courseResponse.data,
+                ...tutorSubject // Aquí se agregan los datos del tutorSubject
+              };
+            })
+          );
+          setCourses(coursesData)
+        } catch (error) {
+          console.error(error);
+        }
+      
+    };
+
     fetchSubject();
-  }, [Cookies.get('session')]);
+    setAdd(false);
+  }, [add]);
 
   const handleClose = () => setShow(false);
   const handleClose2 = () => setShow2(false);
@@ -36,19 +76,46 @@ const MyCourses = () => {
     setSelectedCourse(course);
     setShow(true);
   };
-  const handleShow2 = () => {
-    setShow2(true);
+  const handleShow2 = () => setShow2(true);
+
+  const handleDeleteCourse = async (id) =>{
+    try {
+      await axios.delete(uritutorsubject+'/'+id);
+      toast.success('eliminada exitosamente');
+      setAdd(true);
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+
+
+  const confirmDelete = (id) => {
+    confirmAlert({
+      title: 'Confirmar eliminación',
+      message: '¿Estás seguro de que deseas eliminar el curso?',
+      buttons: [
+        {
+          label: 'Sí',
+          onClick: () => {handleDeleteCourse(id)},
+        },
+        {
+          label: 'No',
+          onClick: () => {},
+        },
+      ],
+    });
   };
 
-  const addCourse = async (courseId) => {
-    const tutorId = Cookies.get('userId'); // Asumiendo que el ID del tutor está guardado en las cookies
-
+  const addCourse = (courseId) => {
+   
     try {
-      await axios.post(`${UriCursos}/add`, {
-        tutorId,
-        courseId,
+      axios.post(uritutorsubject, {
+        "teacher_id" : tutorId,
+        "subject_id": courseId
       });
-      // Aquí puedes actualizar la lista de cursos del tutor si lo deseas
+      // Actualiza la lista de cursos después de agregar un nuevo curso
+      setShow2(false);
+      setAdd(true);
     } catch (error) {
       console.error('Error adding course:', error);
     }
@@ -59,20 +126,21 @@ const MyCourses = () => {
       <div><span className='h3 Principal f_principal'>Tus Cursos</span></div>
       <div className='row justify-content-end py-3'>
         <Button variant="secondary" className='bg_principal Blanco w-25' onClick={handleShow2}>
-          Add new
+          Agregar Nuevo Curso
         </Button>
       </div>
       <div className="row">
         {courses.map(course => (
           <div key={course.id} className="col-md-4 mb-4">
-            <Card>
+            <Card >
               <Card.Body>
+                <img src={pre+course.imagen} alt={course.name} className="card-img-top" style={{height:100,width:300}}/>
                 <Card.Title className='Principal'>{course.name}</Card.Title>
-                <Card.Text className='Secundario'>
-                  {course.description}
-                </Card.Text>
                 <Button variant="primary" className='bg_secundario Blanco' onClick={() => handleShow(course)}>
                   Más información
+                </Button>
+                <Button variant="secondary" className='bg_terciario Negro mx-2' onClick={()=>{confirmDelete(course.id)}}>
+                  Eliminar
                 </Button>
               </Card.Body>
             </Card>
@@ -86,9 +154,21 @@ const MyCourses = () => {
         </Modal.Header>
         <Modal.Body>
           {selectedCourse?.description}
+          <div className='my-2'>
+            <ListGroup>
+                <ListGroup.Item>
+                  <span>aqui van las sesiones ya programadas para este curso de {selectedCourse?.name}</span>
+                </ListGroup.Item>
+            </ListGroup>
+          </div>
+          <div className='my-2 d-flex justify-content-end'>
+            <Button variant="secondary bg_principal Blanco" >
+              Agregar Sesión
+            </Button>  
+          </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose} className='bg_secundario'>
             Cerrar
           </Button>
         </Modal.Footer>
@@ -101,8 +181,13 @@ const MyCourses = () => {
         <Modal.Body>
           <ListGroup>
             {cursosSelect.map(course => (
-              <ListGroup.Item key={course.id} action onClick={() => addCourse(course.id)}>
-                {course.id+".- "}{course.name}
+              <ListGroup.Item key={course.id} >
+                <div className='d-flex justify-content-between'>
+                  <div>{course.id + ".- "}{course.name}</div>
+                <Button variant="secondary bg_principal Blanco" onClick={()=>{addCourse(course.id)}} >
+                  +
+                </Button> 
+                </div>
               </ListGroup.Item>
             ))}
           </ListGroup>

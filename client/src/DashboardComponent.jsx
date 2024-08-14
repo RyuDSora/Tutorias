c0a5a75dcad1f95c35afb4e393938ddcf3157045
carloquer <58191123+carloquer@users.noreply.github.com>
2024-08-14 09:43:01 -0600

    modificaciones responsive

--- client/src/DashboardComponent.jsx
+++ client/src/DashboardComponent.jsx
@@ -7,7 +7,6 @@ import axios from 'axios';
 
 export default function DashboardComponent() {
 
-  // Ejemplo de imágenes locales (colocadas en la carpeta public/images)
   const images = [
     { url: '/images/slide1.png', alt: 'Imagen 1' },
     { url: '/images/slide2.png', alt: 'Imagen 2' },
@@ -18,8 +17,8 @@ export default function DashboardComponent() {
   const courses = [
     { url: '/images/fisicabasica.jpg', alt: 'Curso 1', title: 'Física Básica' },
     { url: '/images/fisicaavanzada.jpg', alt: 'Curso 1', title: 'Física Avanzada' },
-    { url: '/images/matebasica.jpg', alt: 'Curso 2', title: 'Matemáticas Basica' },
-    { url: '/images/mateavanzada.jpg', alt: 'Curso 3', title: 'Matemáticas Avanazada' },
+    { url: '/images/matebasica.jpg', alt: 'Curso 2', title: 'Matemáticas Básica' },
+    { url: '/images/mateavanzada.jpg', alt: 'Curso 3', title: 'Matemáticas Avanzada' },
   ];
 
   const feedbacks = [
@@ -31,9 +30,11 @@ export default function DashboardComponent() {
   return (
     <div className="bg-white">
       <ToastContainer />
-      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
-        <div className="Principal uppercase text-center txt_lg f_regular" style={{ marginTop: '-40px' }}>Tutorías</div>  
-        <div className="Secundario text-center f_principal">Encuentra al tutor perfecto para ti</div>
+      <div className="container py-8">
+        <div className="text-center">
+          <h1 className="Principal uppercase txt_lg f_regular" style={{ marginTop: '-20px' }}>Tutorías</h1>
+          <h2 className="Secundario f_principal">Encuentra al tutor perfecto para ti</h2>
+        </div>
         
         {/* Carrusel de imágenes */}
         <Carousel autoPlay infiniteLoop showThumbs={false} interval={4000}>
@@ -45,56 +46,56 @@ export default function DashboardComponent() {
         </Carousel>
 
         {/* Sección dividida en dos */}
-        <div className="grid grid-cols-2 gap-8 mt-12" style={{ backgroundColor: '#336A41', padding: '20px', borderRadius: '8px' }}>
+        <div className="row mt-5" style={{ backgroundColor: '#336A41', padding: '20px', borderRadius: '8px' }}>
           {/* Lado izquierdo: Video con título */}
-          <div className="col-span-1 flex flex-col justify-center items-center text-white">
+          <div className="col-md-6 text-white d-flex flex-column justify-content-center align-items-center">
             <h3 className="text-xl font-bold mb-4" style={{ fontFamily: 'Arial, sans-serif' }}>Asociate con Nosotros</h3>
-            <video controls width="100%" style={{ maxWidth: '100%', height: 'auto' }}>
+            <video controls className="w-100">
               <source src="/videos/tutorias.mp4" type="video/mp4" />
               Tu navegador no soporta el elemento de video.
             </video>
           </div>
           
           {/* Lado derecho: Imagen */}
-          <div className="col-span-1 flex justify-center items-center">
-            <img src="/images/image4.png" alt="Imagen de la sección derecha" style={{ maxWidth: '100%', height: 'auto' }} />
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
+            <img src="/images/image4.png" alt="Imagen de la sección derecha" className="img-fluid" />
           </div>
         </div>
 
         {/* Imagen debajo de la sección dividida en dos */}
-        <div className="mt-8 flex justify-center">
-          <img src="/images/image5.png" alt="Imagen debajo de la sección dividida" style={{ maxWidth: '100%', height: 'auto' }} />
+        <div className="mt-4 d-flex justify-content-center">
+          <img src="/images/image5.png" alt="Imagen debajo de la sección dividida" className="img-fluid" />
         </div>
 
         {/* Nuevo apartado */}
-        <div className="mt-8 grid grid-cols-2 gap-8 items-center">
+        <div className="row mt-5">
           {/* Imagen a la izquierda */}
-          <div>
-            <img src="/images/image6.jpg" alt="Imagen a la izquierda" style={{ maxWidth: '100%', height: 'auto' }} />
+          <div className="col-md-6">
+            <img src="/images/image6.jpg" alt="Imagen a la izquierda" className="img-fluid" />
           </div>
-          {/* Texto a la derecha */}
-          <div>
-            <h3 className="text-lg text-gray-1000 font-bold" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
-              Aprendizaje que a los estudiantes les encantará <br />
-            </h3>
-            <p className="text-lg text-gray-700" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
-              Una asociación que te encantará <br />
-            </p>
-            <p className="text-lg text-gray-700" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
-              Resultados que todos amarán
-            </p>
-          </div>
+         {/* Texto a la derecha */}
+        <div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
+        <h3 className="text-xl font-bold mb-4 text-center" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
+            Aprendizaje que a los estudiantes les encantará <br />
+          </h3>
+          <p className="text-lg text-center" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
+            Una asociación que te encantará <br />
+          </p>
+          <p className="text-lg text-center" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
+            Resultados que todos amarán
+          </p>
+        </div>
         </div>
 
         {/* Nuevo apartado de feedback */}
-        <div className="feedback-section mt-8">
+        <div className="mt-5">
           <h3 className="text-xl font-bold mb-4 text-center" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>
             Comentarios de nuestros usuarios
           </h3>
-          <div className="feedback-container flex justify-center space-x-4">
+          <div className="d-flex justify-content-center flex-wrap">
             {feedbacks.map((feedback, index) => (
-              <div key={index} className="feedback-card">
-                <img src={feedback.image} alt={feedback.name} className="h-20 w-20 rounded-full mb-4" />
+              <div key={index} className="feedback-card m-3 text-center">
+                <img src={feedback.image} alt={feedback.name} className="rounded-circle mb-2" style={{ width: '200px', height: '200px' }} />
                 <h4 className="font-semibold mb-2">{feedback.name}</h4>
                 <p className="text-gray-700">{feedback.feedback}</p>
               </div>
@@ -103,13 +104,13 @@ export default function DashboardComponent() {
         </div>
 
         {/* Carrusel de cursos */}
-        <div className="courses-section mt-8">
+        <div className="mt-5">
           <h3 className="text-xl font-bold mb-4 text-center" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>Nuestros Cursos</h3>
           <Carousel autoPlay infiniteLoop showThumbs={false} interval={4000}>
             {courses.map((course, index) => (
               <div key={index} className="text-center">
                 <p className="text-lg font-semibold" style={{ fontFamily: 'Clear Sans Light, sans-serif', color: '#336A41' }}>{course.title}</p>
-                <img src={course.url} alt={course.alt} className="course-image" />
+                <img src={course.url} alt={course.alt} className="course-image img-fluid" style={{ maxWidth: '5 0%', height: 'auto' }} />
               </div>
             ))}
           </Carousel>
